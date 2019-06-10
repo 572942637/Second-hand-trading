4be6aae1cbfb907a048161450bdc7f3df5d5a92b
‘e <572942637@qq.com>
2019-06-10 16:31:49 +0800

    web大作业

---
+++ web/src/main/java/com/Controller.java
@@ -0,0 +1,8 @@
+package com;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+}
